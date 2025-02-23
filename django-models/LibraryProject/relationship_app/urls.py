d265138ffa44d93070066abb6337dcdde2e9b3e5
Kingori04 <ngatiaalx@gmail.com>
2025-02-23 20:52:49 +0300

    Importing views

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -1,12 +1,13 @@
 from django.urls import path
-from .views import register
 from .views import list_books
 from .views import LibraryDetailView
 from .views import BookListView
 from .views import user_login
 from .views import user_logout
+from .views import register
 from django.contrib.auth.views import LoginView
 from django.contrib.auth.views import LogoutView 
+from . import views
 
 
 urlpatterns = [
@@ -15,7 +16,7 @@ urlpatterns = [
     path("library/<int:pk>/", LibraryDetailView.as_view(), name="library_detail"),  # CBV for library
     path("login/", LoginView.as_view(template_name="relationship_app/login.html"), name="login"),  # ✅ Fix
     path("logout/", LogoutView.as_view(template_name="relationship_app/logout.html"), name="logout"),  # ✅ Fix
-    path("register/", register, name="register"),
+    path("register/", views.register, name="register"),
     path("", BookListView.as_view(), name="home"),  # ✅ Ensure a "home" URL exists
 ]
 
