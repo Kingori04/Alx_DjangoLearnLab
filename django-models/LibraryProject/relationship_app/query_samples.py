0b65f1d7d9cd323ab24b6050d5b20847c2a5d0f7
Kingori04 <ngatiaalx@gmail.com>
2025-02-23 12:01:18 +0300

    Fixed queries.py to work correctly

--- django-models/LibraryProject/relationship_app/query_samples.py
+++ django-models/LibraryProject/relationship_app/query_samples.py
@@ -11,7 +11,7 @@ from relationship_app.models import Author, Book, Library, Librarian
 def get_books_by_author(author_name):
     try:
         author = Author.objects.get(name=author_name)
-        return author.books.all()
+        return Book.objects.filter(author=author)  # Explicit filter
     except Author.DoesNotExist:
         return []
 
