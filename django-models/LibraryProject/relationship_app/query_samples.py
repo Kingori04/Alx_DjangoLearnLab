e7a70b3133f6565603eac65790e95d3cb24bc943
Kingori04 <ngatiaalx@gmail.com>
2025-02-23 11:58:54 +0300

    Fixed queries.py to work correctly

--- django-models/LibraryProject/relationship_app/query_samples.py
+++ django-models/LibraryProject/relationship_app/query_samples.py
@@ -9,18 +9,27 @@ from relationship_app.models import Author, Book, Library, Librarian
 
 # Query all books by a specific author
 def get_books_by_author(author_name):
-    author = Author.objects.filter(name=author_name).first()
-    return author.books.all() if author else []
+    try:
+        author = Author.objects.get(name=author_name)
+        return author.books.all()
+    except Author.DoesNotExist:
+        return []
 
 # List all books in a library
 def get_books_in_library(library_name):
-    library = Library.objects.filter(name=library_name).first()
-    return library.books.all() if library else []
+    try:
+        library = Library.objects.get(name=library_name)
+        return library.books.all()
+    except Library.DoesNotExist:
+        return []
 
 # Retrieve the librarian for a library
 def get_librarian_for_library(library_name):
-    library = Library.objects.filter(name=library_name).first()
-    return library.librarian if library else None
+    try:
+        library = Library.objects.get(name=library_name)
+        return library.librarian
+    except (Library.DoesNotExist, Librarian.DoesNotExist):
+        return None
 
 # Example Usage
 if __name__ == "__main__":
