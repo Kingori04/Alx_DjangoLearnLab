0719d90722a47671c8fd0aea7bde5760316051b0
Dennis Ngatia <118254990+Kingori04@users.noreply.github.com>
2025-03-01 09:11:29 +0300

    Delete advanced_features_and_security/LibraryProject directory

--- advanced_features_and_security/LibraryProject/bookshelf/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from .models import Book
-
-@admin.register(Book)
-class BookAdmin(admin.ModelAdmin):
-    list_display = ('title', 'author', 'publication_year')  # Columns in the list view
-    search_fields = ('title', 'author')  # Enables searching by title and author
-    list_filter = ('publication_year',)  # Filters by publication year
-
--- advanced_features_and_security/LibraryProject/bookshelf/update.md
+++
@@ -1,8 +0,0 @@
-book = Book.objects.get(title="1984")
-book.title = "Nineteen Eighty-Four"
-book.save()
-
-updated_book = Book.objects.get(id=book.id)
-print(f"Updated Title: {updated_book.title}")
-# Output: Updated Title: Nineteen Eighty-Four
-
